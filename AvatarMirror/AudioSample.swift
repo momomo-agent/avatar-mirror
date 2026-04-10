c383be520b4675aaeb5f6f89ce6c4a81ee752435
momomo-agent <momomo-agent@users.noreply.github.com>
2026-04-10 20:59:00 +0800

    Replace macOS TTS with ElevenLabs: 5 Chinese + 3 English, high quality voices

--- AvatarMirror/AudioSample.swift
+++ AvatarMirror/AudioSample.swift
@@ -9,17 +9,17 @@ struct AudioSample: Identifiable {
     let filename: String
     
     var url: URL? {
-        Bundle.main.url(forResource: filename, withExtension: "caf", subdirectory: "Resources/AudioSamples")
+        Bundle.main.url(forResource: filename, withExtension: "mp3", subdirectory: "Resources/AudioSamples")
     }
     
     static let all: [AudioSample] = [
-        AudioSample(id: "01", name: "早上好", voice: "婷婷", language: "中文", filename: "01_tingting_greeting"),
-        AudioSample(id: "02", name: "看电影", voice: "Shelley", language: "中文", filename: "02_shelley_chat"),
-        AudioSample(id: "03", name: "开会", voice: "Reed", language: "中文", filename: "03_reed_work"),
-        AudioSample(id: "04", name: "搞笑", voice: "Rocko", language: "中文", filename: "04_rocko_funny"),
-        AudioSample(id: "05", name: "温柔", voice: "Sandy", language: "中文", filename: "05_sandy_gentle"),
-        AudioSample(id: "06", name: "Coffee", voice: "Daniel", language: "EN", filename: "06_daniel_english"),
-        AudioSample(id: "07", name: "Amazing", voice: "Flo", language: "EN", filename: "07_flo_english"),
-        AudioSample(id: "08", name: "小猫咪", voice: "爷爷", language: "中文", filename: "08_grandpa_story"),
+        AudioSample(id: "01", name: "早上好", voice: "Amy", language: "中文", filename: "01_amy_greeting"),
+        AudioSample(id: "02", name: "温柔", voice: "Sage", language: "中文", filename: "02_sage_gentle"),
+        AudioSample(id: "03", name: "看电影", voice: "Anna Su", language: "中文", filename: "03_anna_chat"),
+        AudioSample(id: "04", name: "搞笑", voice: "Amy", language: "中文", filename: "04_amy_funny"),
+        AudioSample(id: "05", name: "小猫咪", voice: "Sage", language: "中文", filename: "05_sage_story"),
+        AudioSample(id: "06", name: "Story", voice: "George", language: "EN", filename: "06_george_story"),
+        AudioSample(id: "07", name: "Amazing", voice: "Sarah", language: "EN", filename: "07_sarah_praise"),
+        AudioSample(id: "08", name: "Meeting", voice: "Brian", language: "EN", filename: "08_brian_work"),
     ]
 }
